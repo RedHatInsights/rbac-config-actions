dc18f433d5cd4cbcc181de6c0456aa82190cda0d
wscalf <wscalf@gmail.com>
2024-10-01 10:01:20 -0400

    Initial draft impl of generate-v1-only-permissions action (#15)
    * Initial draft impl of generate-v1-only-permissions action
    
    * Fix copypasta in action file
    
    * Cleanup action files
    
    Signed-off-by: [GitHub] - Automated Action <actions@github.com>
    
    * Use a list of migrated apps instead of individual migrated permissions
    
    * Update action and docs for parameter change
    
    * Fix missed input name
    
    * Add missing defer f.close
    
    ---------
    
    Signed-off-by: [GitHub] - Automated Action <actions@github.com>
    Co-authored-by: [GitHub] - Automated Action <actions@github.com>

---
+++ generate-v1-only-permissions/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"strings"
+
+	v1 "github.com/RedHatInsights/rbac-config-actions/generatepermissions/v1"
+	v2 "github.com/RedHatInsights/rbac-config-actions/generatepermissions/v2"
+)
+
+func main() {
+	kslSrc := flag.String("ksl", "", "The path to the ksl project directory (where the migrated_apps.lst file is)")
+	rbacPermissions := flag.String("rbac-permissions-json", "", "The path to the directory containing RBAC permissions .json files for the current environment.")
+
+	flag.Parse()
+
+	if kslSrc != nil && *kslSrc == "" {
+		kslSrc = nil
+	}
+	if rbacPermissions != nil && *rbacPermissions == "" {
+		rbacPermissions = nil
+	}
+
+	if kslSrc == nil || rbacPermissions == nil {
+		flag.Usage()
+		return
+	}
+
+	migratedApps, err := v2.GetMigratedApps(*kslSrc)
+	if err != nil {
+		panic(err)
+	}
+
+	perms, err := v1.ExtractRBACPermissions(*rbacPermissions, migratedApps)
+	if err != nil {
+		panic(err)
+	}
+
+	for i, perm := range perms {
+		perms[i] = v1PermToV2Perm(perm)
+	}
+
+	v2.WriteV1OnlyPermissionsFile(*kslSrc, perms)
+}
+
+func v1PermToV2Perm(v1Perm string) string {
+	s := strings.ReplaceAll(v1Perm, ":", "_")
+	s = strings.ReplaceAll(s, "-", "_")
+	s = strings.ReplaceAll(s, ".", "_")
+	s = strings.ReplaceAll(s, "*", "all")
+
+	return s
+}
---
+++ generate-v1-only-permissions/v2/V2.go
@@ -0,0 +1,50 @@
+package v2
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+
+	"github.com/project-kessel/ksl-schema-language/pkg/intermediate"
+)
+
+const (
+	V1OnlyPermissionsFile  = "rbac_v1_permissions.json"
+	V2PermissionsExtension = "add_v1_based_permission"
+	ExtensionV1PermName    = "v1_perm"
+)
+
+func GetMigratedApps(kslPath string) (map[string]bool, error) {
+	apps := map[string]bool{}
+
+	listFile, err := os.Open(filepath.Join(kslPath, "migrated_apps.lst"))
+	if err != nil {
+		return apps, err
+	}
+	defer listFile.Close()
+
+	scanner := bufio.NewScanner(listFile)
+	for scanner.Scan() {
+		apps[scanner.Text()] = true
+	}
+
+	return apps, scanner.Err()
+}
+
+func WriteV1OnlyPermissionsFile(kslPath string, perms []string) error {
+	refs := []*intermediate.ExtensionReference{}
+
+	for _, perm := range perms {
+		refs = append(refs, &intermediate.ExtensionReference{Namespace: "rbac", Name: "add_v1only_permission", Params: map[string]string{"perm": perm}})
+	}
+
+	ns := &intermediate.Namespace{Name: "rbac_v1_permissions", Imports: []string{"rbac"}, ExtensionReferences: refs}
+
+	f, err := os.OpenFile(filepath.Join(kslPath, "src", V1OnlyPermissionsFile), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return intermediate.Store(ns, f)
+}
