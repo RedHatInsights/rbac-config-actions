447d70c519c350b4b4a00bf3427b6aba620ad7a4
[GitHub] - Automated Action <actions@github.com>
2024-10-01 08:37:44 -0400

    iterate through envs
    Signed-off-by: [GitHub] - Automated Action <actions@github.com>

--- convert-config/entrypoint.sh
+++ convert-config/entrypoint.sh
@@ -76,14 +76,14 @@ go build -gcflags "all=-N -l" -o ./bin/ ./...
 cd ..
 
 # Run KSL compiler
+for ENV in stage prod
+do # ex. configs/stage/schemas
+  ./ksl-schema-language/bin/ksl -o configs/${ENV}/schemas/schema.zed configs/${ENV}/schemas/src/*.ksl configs/${ENV}/schemas/src/*.json
+  rm configs/${ENV}/schemas/src/rbac_v1_permissions.json
+done
 
-./ksl-schema-language/bin/ksl -o ${INPUT_KSL_SRC_STAGE}/schema.zed ${INPUT_KSL_SRC_STAGE}/src/*.ksl ${INPUT_KSL_SRC_STAGE}/src/*.json
-./ksl-schema-language/bin/ksl -o ${INPUT_KSL_SRC_PROD}/schema.zed ${INPUT_KSL_SRC_PROD}/src/*.ksl ${INPUT_KSL_SRC_PROD}/src/*.json
-
-# Remove KSL repo & ksil files
+# Remove KSL repo
 rm -rf ksl-schema-language/
-rm ${INPUT_KSL_SRC_STAGE}/src/rbac_v1_permissions.json
-rm ${INPUT_KSL_SRC_PROD}/src/rbac_v1_permissions.json
 
 # push the changes
 git add .
