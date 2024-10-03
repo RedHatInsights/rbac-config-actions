53dd1c8b317b03d8ea86fabd730dac7d3cf27b68
[GitHub] - Automated Action <actions@github.com>
2024-10-03 16:20:46 -0400

    Exit if error generating schema
    Signed-off-by: [GitHub] - Automated Action <actions@github.com>

--- validate-schema/entrypoint.sh
+++ validate-schema/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -l
 
-set -e
-
 # generate schemas
 # Clone ksl-schema-language repo
 git clone --depth=1 https://github.com/project-kessel/ksl-schema-language.git
@@ -15,7 +13,7 @@ cd ..
 # Run KSL compiler
 for ENV in stage prod
 do # ex. configs/stage/schemas
-  ./ksl-schema-language/bin/ksl -o configs/${ENV}/schemas/schema.zed configs/${ENV}/schemas/src/*.ksl configs/${ENV}/schemas/src/*.json
+  ./ksl-schema-language/bin/ksl -o configs/${ENV}/schemas/schema.zed configs/${ENV}/schemas/src/*.ksl configs/${ENV}/schemas/src/*.json || exit 1
   rm configs/${ENV}/schemas/src/rbac_v1_permissions.json
 done
 
